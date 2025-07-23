e19a2bc5825f95aa760329327bc08c86137e4ac0
Hanifi Korkmaz <korkmaz.hanifi@outlook.com>
2025-07-23 10:25:53 +0300

    Fragmentlar için binding ayarlamaları gerçekleştirildi

--- app/src/main/java/com/hanifikorkmaz/fragmentandnavigationlearn/FirstFragment.kt
+++ app/src/main/java/com/hanifikorkmaz/fragmentandnavigationlearn/FirstFragment.kt
@@ -5,22 +5,37 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hanifikorkmaz.fragmentandnavigationlearn.databinding.FragmentFirstBinding
 
 
 class FirstFragment : Fragment() {
 
+    private var _binding: FragmentFirstBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+
+        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 
 }
--- app/src/main/java/com/hanifikorkmaz/fragmentandnavigationlearn/SecondFragment.kt
+++ app/src/main/java/com/hanifikorkmaz/fragmentandnavigationlearn/SecondFragment.kt
@@ -5,9 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.hanifikorkmaz.fragmentandnavigationlearn.databinding.FragmentSecondBinding
 
 
 class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,9 +23,20 @@ class SecondFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false)
+
+        _binding= FragmentSecondBinding.inflate(inflater, container, false)
+        val view= binding.root
+        return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding= null
+    }
 
 }
